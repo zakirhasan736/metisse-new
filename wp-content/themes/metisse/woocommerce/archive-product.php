74db79ceb275d3b68747d82acda4495bad55d131
Md Zakir Hosen <59880204+zakirhasan736@users.noreply.github.com>
2024-08-08 09:16:10 +0100

    updated

--- wp-content/themes/metisse/woocommerce/archive-product.php
+++ wp-content/themes/metisse/woocommerce/archive-product.php
@@ -87,7 +87,7 @@ $category = isset($_GET['category']) ? $_GET['category'] : '';
 		<div class="col-span-full">
 			<div class="custom-container-fluid">
 				<header class="woocommerce-products-header">
-					<div class="custom-container">
+					<div class="custom-container-fluid">
 
 						<?php
 						/**
