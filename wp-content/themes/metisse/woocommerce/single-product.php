8b7a381242588ed673ce9cbeb362b884583e33a8
Md Zakir Hosen <59880204+zakirhasan736@users.noreply.github.com>
2024-08-10 10:33:39 +0100

    updated new flow

--- wp-content/themes/metisse/woocommerce/single-product.php
+++ wp-content/themes/metisse/woocommerce/single-product.php
@@ -67,68 +67,112 @@ do_action('woocommerce_sidebar');
 
 
 
-<section class="product-category-section pb-[95px] lg:pb-[80px] sm:pb-[60px]">
-	<div class="custom-container">
-		<div class="section-title-box mb-8">
-			<h2 class="section-title text-[16px] text-black text-center font-bold font-secondary tracking-[3.2px] uppercase">Categories</h2>
+<section class="product-feature-type-featured-product overflow-hidden pt-[76px] sm:pt-[60px] pb-[114px] lg:pb-[90px] md:pb-[80px] sm:pb-[60px]">
+	<div class="custom-container-site-width">
+		<div class="product-new-arrival-left-cont flex items-center pt-[114px] pb-[112px] justify-end pr-[115px] w-full max-w-full min-w-[442px]">
+			<img src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/company-brand-logo-black.svg" alt="product modal brand logo image">
 		</div>
-		<div class="product-category-wrapper">
-			<div class="grid grid-rows-2 grid-cols-12 gap-x-[33px] gap-y-[30px] md:gap-4">
-				<?php
-				// Get highlighted categories
-				$highlighted_categories = get_field('highlighted_categories', 'option');
+		<div class="new-arrival-product-feature-watches">
+			<div class="product-feature-watches-type-new-arrivel-wrap relative w-full">
+				<div class="product-new-arrival-slider w-full !mb-0">
+					<?php
+					// Query to fetch all products
+					$args = array(
+						'post_type' => 'product',
+						'posts_per_page' => -1, // Display all products
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'product_cat', // Taxonomy name
+								'field' => 'slug', // Select taxonomy term by slug
+								'terms' => 'New Arrival', // Slug of the "featured" category
+							),
+						),
+					);
+					$products_query = new WP_Query($args);
 
-				// Loop through each highlighted category
-				if ($highlighted_categories) :
-					$displayed_categories_count = 0;
-					foreach ($highlighted_categories as $category) :
-						$displayed_categories_count++;
-						if ($displayed_categories_count > 3) {
-							break; // Exit the loop if three categories are displayed
-						}
-						// Determine row and column spans
-						$row_span = ($displayed_categories_count === 1) ? 'row-span-2' : 'row-span-1';
-						$col_span = 'col-span-6 sm:col-span-full';
-				?>
-						<div class="<?php echo $row_span; ?> <?php echo $col_span; ?> h-full">
-							<div class="product-category-card-item relative h-full">
-								<div class="category-img-box relative h-full">
-									<?php
-									// Get category thumbnail URL
-									$thumbnail_id = get_term_meta($category->term_id, 'thumbnail_id', true);
-									$image_src = wp_get_attachment_url($thumbnail_id);
-									?>
-									<img class="h-full w-full object-cover" src="<?php echo $image_src; ?>" alt="<?php echo $category->name; ?>">
-									<div class="cat-image-overlyn absolute top-0 left-0 w-full h-full"></div>
-								</div>
-								<div class="category-content text-center absolute bottom-0 flex justify-end items-center flex-col p-10 md:p-[25px] left-0 right-0 mx-x-auto w-full h-full">
-									<h3 class="cat-title <?php echo ($category->description) ? 'mb-[6px]' : 'mb-[14px]'; ?> leading-none text-[26px] md:text-[20px] sm:text-[18px] text-white opacity-80 font-accend font-bold capitalize text-center "><?php echo $category->name; ?></h3>
-									<?php if ($category->description) : ?>
-										<p class="cat-desc opacity-50 text-[12px] text-center font-primary font-medium text-white mb-[15px] block"><?php echo $category->description; ?></p>
-									<?php endif; ?>
-
-									<a href="<?php echo get_term_link($category); ?>" class="product-category-item py-[14px] px-[31px] bg-[#FFFFFFF2] text-[14px] text-center font-primary font-medium text-[#000] h-[45px] flex items-center justify-center">Shop Now</a>
+					if ($products_query->have_posts()) :
+						while ($products_query->have_posts()) : $products_query->the_post();
+					?>
+							<div class="product--card-item relative" style="margin-right: 24px;">
+								<div class="product--card-main-cont flex items-start gap-[20px]">
+									<div class="product--img-box h-[286px] w-[163px] relative">
+										<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full', array('class' => 'product-img mb-[24px] md:mb-5 sm:mb-4 w-full h-[270px] object-cover')); ?></a>
+									</div>
+									<div class="product--card-cont pt-5 pb-[22px]">
+										<h3 class="product-title leading-none mb-[7px] text-[14px] text-left font-primary font-normal tracking-[1.4px] uppercase text-[#131313]"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+										<p class="product-desc text-[12px] text-left font-secondary font-normal leading-normal mb-[29px]"><?php echo wp_trim_words(get_the_excerpt(), 6); ?></p>
+										<div class="product-verient-box mb-[42px]">
+											<ul class="p-varient-lists flex items-center gap-[6px] justify-start">
+												<li class="w-[16px] h-[16px] rounded-full bg-[#D9D9D9] border-2 border-[#000]"></li>
+												<li class="w-[16px] h-[16px] rounded-full bg-[#FFE6E6] active:bg-[#D9D9D9] border-2 border-transparent active:border-[#000]"></li>
+											</ul>
+										</div>
+										<div class="product--features-watches-btn-box text-left relative left-0 w-full h-full flex flex-col items-start justify-start">
+											<p class="product-price text-[29px] text-left font-secondary font-normal capitalize text-black mb-[14px]"><?php echo get_woocommerce_currency_symbol() . get_post_meta(get_the_ID(), '_price', true); ?></p>
+											<button class="add-to-cart-btn mx-auto flex items-center justify-center w-full whitespace-nowrap h-[40px] py-2 px-[0px] border-0 capitalize text-[#BD7048] text-[12px] font-semibold text-center font-secondary leading-[1.2]"> <?php woocommerce_template_loop_add_to_cart('Shop now', 'metisse'); ?></button>
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-				<?php
-					endforeach;
-				endif;
-				?>
+					<?php
+						endwhile;
+						wp_reset_postdata();
+					endif;
+					?>
+				</div>
+				<div class="product-slider-arrow-box relative mt-10">
+					<button class="slick-prev-arrow custom-arrow custom-prev-new-arrv custom-prev absolute bottom-0 right-[54px] transform z-10 w-10 h-10 flex items-center justify-center">
+						<i class="fa fa-angle-left text-lg text-[#131313]"></i>
+					</button>
+					<button class="slick-next-arrow custom-arrow custom-next-new-arrv custom-next absolute bottom-0 right-[0px] transform z-10 w-10 h-10 flex items-center justify-center">
+						<i class="fa fa-angle-right text-lg text-[#131313]"></i>
+					</button>
+				</div>
 			</div>
 		</div>
+
 	</div>
 </section>
 
-<section class="blogs-and-article-section">
-	<?php
-	// Get the Elementor template shortcode
-	$elementor_template_shortcode = '[elementor-template id="168"]';
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		// Ensure the slider is initialized after the DOM is fully loaded and arrows are properly placed
+		setTimeout(function() {
+			$('.product-new-arrival-slider').slick({
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+				arrows: true,
+				prevArrow: $('.custom-prev-new-arrv'),
+				nextArrow: $('.custom-next-new-arrv'),
+				responsive: [{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 1,
+							infinite: true,
+							dots: true
+						}
+					},
+					{
+						breakpoint: 600,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1
+						}
+					}
+				]
+			});
 
-	// Display the Elementor template using do_shortcode
-	echo do_shortcode($elementor_template_shortcode);
-	?>
-</section>
+			// Remove the right margin on the last item to prevent overflow
+			$('.product-new-arrival-slider').on('setPosition', function() {
+				$(this).find('.product--card-item').css('margin-right', '24px');
+				$(this).find('.product--card-item:last-child').css('margin-right', '0');
+			});
+		}, 300); // Adding delay to ensure everything is rendered properly
+	});
+</script>>
 
 
 <?php
