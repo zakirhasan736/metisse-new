3d5c042cb789a3cd09f4624a8bf33d0c2789ef7b
Md Zakir Hosen <59880204+zakirhasan736@users.noreply.github.com>
2024-08-10 04:12:50 +0100

    updated new flow

--- wp-content/themes/metisse/home.php
+++ wp-content/themes/metisse/home.php
@@ -37,30 +37,24 @@ get_header();
                 if ($products_query->have_posts()) :
                     while ($products_query->have_posts()) : $products_query->the_post();
                 ?>
-                        <div class="col-span-3 md:col-span-4 sm:col-span-full">
-                            <div class="product-card-item border-2 border-[#0000001a] bg-white relative">
-                                <div class="product-card-main-cont">
-                                    <div class="product-img-box h-[270px] relative mb-[24px] md:mb-5 sm:mb-4">
-                                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full', array('class' => 'product-img mb-[24px] md:mb-5 sm:mb-4 w-full h-[270px] object-cover')); ?></a>
-
-                                        <div class="product-card-checkout-btns text-center absolute left-0 w-full h-full flex flex-col items-center justify-center">
-                                            <button class="add-to-cart-btn max-w-[208px] mx-auto flex items-center justify-center w-full whitespace-nowrap h-[45px] py-[14px] px-[20px] border-2 border-[#000000F2] capitalize text-black text-[14px] font-medium text-center mb-[5px] font-primary leading-[1.2] bg-white"> <?php woocommerce_template_loop_add_to_cart('Shop now', 'metisse'); ?></button>
-                                            <a href="<?php echo esc_url(WC()->cart->get_checkout_url()); ?>" class="buy-now-btn max-w-[208px] mx-auto flex items-center justify-center w-full whitespace-nowrap h-[45px] py-[14px] px-[20px] border-2 border-[#000000F2] capitalize text-white text-[14px] font-medium text-center font-primary leading-[1.2] bg-[#000000F2]"><?php echo esc_html__('Buy Now', 'metisse'); ?></a>
-                                        </div>
+                        <div class="product--card-item relative" style="margin-right: 24px;">
+                            <div class="product--card-main-cont flex items-start gap-[20px]">
+                                <div class="product--img-box h-[286px] w-[163px] relative">
+                                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full', array('class' => 'product-img mb-[24px] md:mb-5 sm:mb-4 w-full h-[270px] object-cover')); ?></a>
+                                </div>
+                                <div class="product--card-cont pt-5 pb-[22px]">
+                                    <h3 class="product-title leading-none mb-[7px] text-[14px] text-left font-primary font-normal tracking-[1.4px] uppercase text-[#131313]"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                    <p class="product-desc text-[12px] text-left font-secondary font-normal leading-normal mb-[29px]"><?php echo wp_trim_words(get_the_excerpt(), 6); ?></p>
+                                    <div class="product-verient-box mb-[42px]">
+                                        <ul class="p-varient-lists flex items-center gap-[6px] justify-start">
+                                            <li class="w-[16px] h-[16px] rounded-full bg-[#D9D9D9] border-2 border-[#000]"></li>
+                                            <li class="w-[16px] h-[16px] rounded-full bg-[#FFE6E6] active:bg-[#D9D9D9] border-2 border-transparent active:border-[#000]"></li>
+                                        </ul>
                                     </div>
-                                    <div class="product-card-cont px-[16.5px] pb-[22px]">
-                                        <h3 class="product-title leading-none mb-[7px] text-[18px] text-center font-primary font-bold capitalize text-black"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                                        <p class="product-desc text-[16px] text-center font-primary font-normal leading-normal mb-[14px]"><?php echo wp_trim_words(get_the_excerpt(), 6); ?></p>
-                                        <p class="product-price text-[18px] text-center font-primary font-bold capitalize text-black mb-[14px]"><?php echo get_woocommerce_currency_symbol() . get_post_meta(get_the_ID(), '_price', true); ?></p>
-                                        <div class="product-verient-box">
-                                            <p class="varient-title text-[10px] text-center text-black opacity-50 mb-[7px] font-secondary font-semibold tracking-[1.8px] uppercase leading-none">Colour Variants</p>
-                                            <ul class="p-varient-lists flex items-center gap-[6px] justify-center">
-                                                <li class="w-[18px] h-[18px] rounded-full bg-[#D9D9D9] border-2  border-[#000]"></li>
-                                                <li class="w-[18px] h-[18px] rounded-full bg-[#FFE6E6] active:bg-[#D9D9D9] border-2 border-transparent active:border-[#000]"></li>
-                                            </ul>
-                                        </div>
+                                    <div class="product--features-watches-btn-box text-left relative left-0 w-full h-full flex flex-col items-start justify-start">
+                                        <p class="product-price text-[29px] text-left font-secondary font-normal capitalize text-black mb-[14px]"><?php echo get_woocommerce_currency_symbol() . get_post_meta(get_the_ID(), '_price', true); ?></p>
+                                        <button class="add-to-cart-btn mx-auto flex items-center justify-center w-full whitespace-nowrap h-[40px] py-2 px-[0px] border-0 capitalize text-[#BD7048] text-[12px] font-semibold text-center font-secondary leading-[1.2]"> <?php woocommerce_template_loop_add_to_cart('Shop now', 'metisse'); ?></button>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
@@ -116,9 +110,9 @@ get_header();
     <div class="custom-container-new">
         <div class="section-title-box mb-14 flex justify-between items-center gap-4 border-b border-[#DCDCDC]">
             <h2 class="section-title text-[14px] text-[#5A5A5A] text-center font-semibold pb-[9px] font-secondary leading-normal uppercase border-b-[3px] border-b-[#717171]">Featured Watches</h2>
-            <a href="/shope/" class="secondary-btn-view-all-btn text-[12px] font-normal leading-normal mb-[9px] font-secondary text-[#131313] flex justify-center items-center px-8 h-12 py-2 rounded-[4px] border border-[rgb(19,19,19, .6)]">View all</a>
+            <a href="/shop/" class="secondary-btn-view-all-btn text-[12px] font-normal leading-normal mb-[9px] font-secondary text-[#131313] flex justify-center items-center px-8 h-12 py-2 rounded-[4px] border border-[rgb(19,19,19, .6)]">View all</a>
         </div>
-        <div class="product-feature-type-new-arrivel-wrap">
+        <div class="product-feature-type-new-arrivel-wrap relative">
             <div class="product-slider !mb-0">
                 <?php
                 // Query to fetch all products
@@ -135,7 +129,6 @@ get_header();
                 );
                 $products_query = new WP_Query($args);
 
-
                 if ($products_query->have_posts()) :
                     while ($products_query->have_posts()) : $products_query->the_post();
                 ?>
@@ -149,7 +142,7 @@ get_header();
                                     <p class="product-desc text-[12px] text-left font-secondary font-normal leading-normal mb-[29px]"><?php echo wp_trim_words(get_the_excerpt(), 6); ?></p>
                                     <div class="product-verient-box mb-[42px]">
                                         <ul class="p-varient-lists flex items-center gap-[6px] justify-start">
-                                            <li class="w-[16px] h-[16px] rounded-full bg-[#D9D9D9] border-2  border-[#000]"></li>
+                                            <li class="w-[16px] h-[16px] rounded-full bg-[#D9D9D9] border-2 border-[#000]"></li>
                                             <li class="w-[16px] h-[16px] rounded-full bg-[#FFE6E6] active:bg-[#D9D9D9] border-2 border-transparent active:border-[#000]"></li>
                                         </ul>
                                     </div>
@@ -158,7 +151,6 @@ get_header();
                                         <button class="add-to-cart-btn mx-auto flex items-center justify-center w-full whitespace-nowrap h-[40px] py-2 px-[0px] border-0 capitalize text-[#BD7048] text-[12px] font-semibold text-center font-secondary leading-[1.2]"> <?php woocommerce_template_loop_add_to_cart('Shop now', 'metisse'); ?></button>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                 <?php
@@ -167,10 +159,14 @@ get_header();
                 endif;
                 ?>
             </div>
+            <!-- Custom arrows for slider navigation -->
+            <button class="slick-prev-arrow custom-arrow custom-prev absolute top-1/2 left-[-20px] transform -translate-y-1/2 z-10 w-10 h-10 flex items-center justify-center bg-gray-200 rounded-full shadow-lg">
+                <i class="fa fa-angle-left text-lg text-[#131313]"></i>
+            </button>
+            <button class="slick-next-arrow custom-arrow custom-next absolute top-1/2 right-[-20px] transform -translate-y-1/2 z-10 w-10 h-10 flex items-center justify-center bg-gray-200 rounded-full shadow-lg">
+                <i class="fa fa-angle-right text-lg text-[#131313]"></i>
+            </button>
         </div>
-        <!-- Add arrows for slider navigation -->
-        <div class="slick-prev-arrow"></div>
-        <div class="slick-next-arrow"></div>
     </div>
 </section>
 
@@ -182,8 +178,8 @@ get_header();
             infinite: true,
             dots: true,
             arrows: true,
-            prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
-            nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+            prevArrow: $('.custom-prev'),
+            nextArrow: $('.custom-next'),
             responsive: [{
                     breakpoint: 1024,
                     settings: {
